8847b14fadb17a1d7a1d0ea27250f69800df89ec
Jrod595 <jsza595@gmail.com>
2022-02-10 11:24:37 -0800

    assignment3-2.cpp

--- assignment3-2.cpp
+++ assignment3-2.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main ()
 {
-    int numMale;
-    int numFemale;
+    int males, females, total;
+    double percentage;
+
+    cout << "How many males are there in the class? ";
+    cin >> males;
+    cout << "How many females are there in the class? ";
+    total = males + females;
+    percentage = (double)males / total;
+    cout << setprecision(2) << "Percentage of males is: ";
+    cout << percentage * 100 << "%";
+    return 0;
 }
