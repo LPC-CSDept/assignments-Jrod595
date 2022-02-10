4fd4ac4bc683de0c5053c00a6525ff880cc8f0c9
Jrod595 <jsza595@gmail.com>
2022-02-10 10:47:19 -0800

    assignment3-1.cpp

--- newcode1.cpp
+++ newcode1.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+using namespace std;
+
 int main()
 {
-     std::cout << "Hello World" << std::endl;
+     char c1='0';
+     char c2='1';
+     char c3 = c1 + c2;
+     cout << c3 << endl;
 }
