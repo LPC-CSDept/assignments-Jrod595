a2cb152fa21a01c37ef3fb1c3688430117b4f894
Jrod595 <jsza595@gmail.com>
2022-03-22 14:44:10 -0700

    question2quiz2

---
+++ quiz2q2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ int number, exponent, i,ans;
+  ans=1;
+  i=1;
+  cout<<"Enter a number  : ";
+  cin>>number;
+  cout<<"Enter exponent  : ";
+  cin>>exponent;
+  do
+    {ans=ans*number;
+      i++;
+      }
+    while(i<+exponent);
+  cout<<number<<"^"<<exponent<<"="<<ans;
+  return 0;
+}
