97ceb2f4e6fc1736cbdb5b661ff66cd90ba49312
Jrod595 <jsza595@gmail.com>
2022-03-22 13:38:52 -0700

    5-2 process

---
+++ 5-2quiz.cpp
@@ -0,0 +1,10 @@
+#include <cstdlib>
+#include <ctime>
+#int randomGen()
+int main()
+{
+srand(time(0));
+random = rand( ) ;
+  int random [5], max_value = 99, min_value = 0;
+  random = rand() % max_value + min_value;
+  
