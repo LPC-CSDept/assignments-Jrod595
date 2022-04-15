e8c4c61b00bfc555f2495aecc8afb35ffdbb7bcd
Jrod595 <jsza595@gmail.com>
2022-04-15 16:41:00 -0700

    6-1 q1 finished

--- 6-1quiz.cpp
+++ 6-1quiz.cpp
@@ -13,3 +13,19 @@ void swapTwoValues(int n1, int n2) {
   cout << "after INT swapping." << endl;
   cout << "n1 = " << n1 << "n2 = " << n2 << endl;
 }
+void swapTwoValues(double  f1, double f2) {
+  cout << "before FLOAT swapping.";
+  double f3;
+  f3 = f1;
+  f1=f2;
+  f2=f3;
+  
+  cout << " after FLOAT swapping. " << f1 << endl;
+}
+
+int main() {
+  swapTwoValues(6, 9);
+  swapTwoValues(6.9, 4.2);
+
+  return 0;
+}
