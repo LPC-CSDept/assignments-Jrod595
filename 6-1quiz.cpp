c677f313990d1f67d8db05417e77c9b5942d6e7d
Jrod595 <jsza595@gmail.com>
2022-04-15 16:28:10 -0700

    6-1 second commit

--- 6-1quiz.cpp
+++ 6-1quiz.cpp
@@ -19,3 +19,17 @@ void swapTwoValues(int n1, int n2) {
 
   cout << "after INT swapping." << endl;
   cout << "n1 = " << n1 << "n2 = " << n2 << endl;
+  }
+void swapTwoValues(float f1, float f2) {
+  float temp;
+
+ cout << "before FLOAT swapping." << endl;
+  cout << "f1 = " << f1 << "f2 = " << f2 << endl;
+
+  temp = f1;
+  f1 = f2;
+  f2 = temp;
+
+  cout << "after FLOAT swapping." << endl;
+  cout << "f1 = " << f1 << "f2 = " << f2 << endl;
+}
