715bafba7a9e675bb509903b272fbcda7af1fdf7
Jrod595 <jsza595@gmail.com>
2022-04-15 17:45:22 -0700

    6-1 commit change

--- 6-1question2.cpp
+++ 6-1question2.cpp
@@ -11,36 +11,15 @@ std::tuple<int, int, int> generateNumbs() {
   srand(time(0));
 
     n1 = rand() % 100;
-    // std::cout << n1 << endl;
+    std::cout << n1 << endl;
 
     n2 = rand() % 100;
-    // cout << n2 << endl;
+    std::cout << n2 << endl;
 
     n3 = rand() % 100;
-    // cout << n3 << endl;
+    std::cout << n3 << endl;
+  
   return  {n1,n2,n3};
-}
-
-
-
-int main()  {
-  // func to create rand numbers that returns an array
-  // arr = generateNumbs();
-  auto [n1, n2, n3] = generateNumbs();
-  
-  
-  // func to sort array
-  // min = findMin(n1,n2,n3);
-  
-  // func to cout eveyrthing 
-  printMin(n1,n2,n3);
-
-  
-  
-  
-  // min = findMin(n1,n2,n3);
-  
-  return 0;
 }
 
 int min(int n1, int n2, int n3) {
@@ -50,11 +29,19 @@ int min(int n1, int n2, int n3) {
   arr[2] = n3;
   
   std::sort(arr, arr+sizeof(arr));
-  
   return arr[0];
 } 
 
 int printMin(int n1, int n2, int n3) {
   cout << "min(n1,n2,n3)==" << min(n1,n2,n3) << '\n';
+  return 0;
 }
+
+int main()  {
+  // func to create rand numbers that returns an array
+  // arr = generateNumbs();
+  auto [n1, n2, n3] = generateNumbs();
+  printMin(n1,n2,n3);
   
+  return 0;
+}
