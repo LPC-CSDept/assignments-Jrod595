719afc4d59630531d2d073b1b57629b62dcce7ae
Jrod595 <jsza595@gmail.com>
2022-04-15 17:39:15 -0700

    push 6-1 q2

--- 6-1question2.cpp
+++ 6-1question2.cpp
@@ -2,20 +2,44 @@
 #include <cstdlib>
 #include <ctime>
 #include <iomanip>
-
+#include <tuple>
+#include <algorithm>
 using namespace std;
 
-int main() 
-{
-  int n1, n2, n3;
-   srand(time(0));
+std::tuple<int, int, int> generateNumbs() {
+  int n1,n2,n3;
+  srand(time(0));
 
     n1 = rand() % 100;
-    cout << n1 << endl;
+    // std::cout << n1 << endl;
 
     n2 = rand() % 100;
-    cout << n2 << endl;
+    // cout << n2 << endl;
 
     n3 = rand() % 100;
-    cout << n3 << endl;
+    // cout << n3 << endl;
+  return  {n1,n2,n3};
+}
+
+
+
+int main()  {
+  // func to create rand numbers that returns an array
+  // arr = generateNumbs();
+  auto [n1, n2, n3] = generateNumbs();
+  
+  
+  // func to sort array
+  // min = findMin(n1,n2,n3);
+  
+  // func to cout eveyrthing 
+  printMin(n1,n2,n3);
+
+  
+  
+  
+  // min = findMin(n1,n2,n3);
+  
+  return 0;
+}
   
