7f39e8d6c67975283600d54af5d8bbb1b6e5168e
Jrod595 <jsza595@gmail.com>
2022-04-19 21:22:22 -0700

    midterm q 2 push

--- midterm/midtermq2.cpp
+++ midterm/midtermq2.cpp
@@ -1,14 +1,24 @@
-#include<iostream>
- #include<fstream>
-#include<cmath>
-#include<cstdlib>
+#include <iostream>
+ #include <fstream>
+#include <cmath>
+#include <cstdlib>
 using namespace std;
 
 int main()
 {
 char filename [50];
 ifstream students;
-cin.getline (filename, 50)
+cin.getline (filename, 50);
+students.open(filename);
 
+if(!students.is_open()){
+    exit (EXIT_FAILURE);
+}
+char word[50];
+students >> word;
+while (students.good()){
+    cout << word << " ";
+    students >> word;
+}
 
 }
