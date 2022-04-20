bcf759b7dfff1ce6c0b229463b5740c2f9ef6fcc
Jrod595 <jsza595@gmail.com>
2022-04-19 20:56:56 -0700

    first commit

---
+++ midterm/midtermq2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+ #include<fstream>
+ #include<string>
+ #include<iomanip>
+ using namespace std;
+ int main()
+ {
+     struct studentType
+     {
+
+     //Declare variables
+
+     string studentFName;
+     string studentMName;
+
+     string studentLName;
+     
+
+     int testScore;
+
+     char grade;
+
+ }
