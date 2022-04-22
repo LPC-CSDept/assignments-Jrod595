2c46d8d8d786b8f45d9624d1cc5d0737acea8f90
Jrod595 <jsza595@gmail.com>
2022-04-21 20:58:15 -0700

    push two take 2 midterm

--- Midterm/t2q2midterm.cpp
+++ Midterm/t2q2midterm.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 int main () {
-  ofstream myfile ("studentsS.txt");
+  string line;
+  ifstream myfile ("students.txt");
   if (myfile.is_open())
   {
-    myfile << "This is a line.\n";
-    myfile << "This is another line.\n";
+    while ( getline (myfile,line) )
+    {
+      cout << line << '\n';
+    }
     myfile.close();
   }
-  else cout << "Unable to open file";
+
+  else cout << "Unable to open file"; 
+
   return 0;
 }
