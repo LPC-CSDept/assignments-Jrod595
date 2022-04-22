7f04370f8adcb1ad0471f70187ae1636d914ad10
Jrod595 <jsza595@gmail.com>
2022-04-21 21:23:05 -0700

    push for midterm take 2

--- Midterm/t2q2midterm.cpp
+++ Midterm/t2q2midterm.cpp
@@ -2,15 +2,28 @@
 #include <fstream>
 #include <string>
 using namespace std;
-
+bool isNumber(const string& str)
+{
+    for (char const &c : str) {
+        if (std::isdigit(c) == 0) return false;
+    }
+    return true;
+}
 int main () {
   string line;
   ifstream myfile ("students.txt");
   if (myfile.is_open())
   {
+    string studentname;
+    int total80;
+    
     while ( getline (myfile,line) )
     {
-      cout << line << '\n';
+     if (!isNumber(line)){
+       studentname=line;
+      continue;
+     }
+       
     }
     myfile.close();
   }
