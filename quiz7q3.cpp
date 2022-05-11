05d7ca69a58e2235b9483c44d1269af43cd070f5
Jrod595 <jsza595@gmail.com>
2022-05-10 21:48:57 -0700

    quiz7q3 push

---
+++ quiz7q3.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int largest(int arr[], int n)
+{
+    int i;
+     
+    // Initialize maximum element
+    int max = arr[0];
+ 
+    // look over array elements
+    // from second and compare
+    // every element with current max
