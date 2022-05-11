172a3e4bb5ebc36b6b1c568f8d03ab4eca6eebda
Jrod595 <jsza595@gmail.com>
2022-05-10 21:12:02 -0700

    quiz7 q2 push

--- quiz7q1.cpp
+++ quiz7q1.cpp
@@ -9,3 +9,13 @@ return true;
 //checks to see if index is N-1 then checks only with previous element
 if(index==N-1 && (abs(a[index]-a[index-1])<10))
 return true;
+//otherwise checks for the previous and next element
+if(index!=N && index!=0 && (abs(a[index]-a[index+1])<10) && (abs(a[index]-a[index-1])<10))
+return true;
+//return false if condition is not satisfied
+return false;
+}
+int main(){
+int number[N] = {10,15,0,-25,19,20,25,30,45,20};
+//lopp from 0 to less than N
+for(int i=0;i<N;i++){
