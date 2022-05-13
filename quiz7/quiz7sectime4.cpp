ac6a5a87ed2626d708da317476f8191c722a0799
Jrod595 <jsza595@gmail.com>
2022-05-13 12:59:21 -0700

    push q 4

--- quiz7/quiz7sectime4.cpp
+++ quiz7/quiz7sectime4.cpp
@@ -6,7 +6,8 @@ using namespace std;
 void deleteOne(vector<int> &);
 int main()
 {
-vector<int> v {10, 20, 30, 20, 40, 20};
+    //elements that can be chosen to be erased
+vector<int> v {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
   
 deleteOne(v);
 for (int i = 0; i < v.size(); i++) {
@@ -18,3 +19,13 @@ return 0;
 
 void deleteOne(vector<int> &v)
 {
+    // allow user to pick element (num) to be erased/deleted
+int usernum;
+cout<<"Enter element to be deleted: ";
+cin>>usernum;
+  
+vector<int>::iterator it;
+  
+it = std::find(v.begin(), v.end(), usernum);
+int deletecnt = 0;
+while(it!= v.end())
