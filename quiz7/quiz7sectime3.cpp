84d087c63f2fe9698c523874114617733e94d407
Jrod595 <jsza595@gmail.com>
2022-05-13 12:06:37 -0700

    push change q3

--- quiz7/quiz7sectime3.cpp
+++ quiz7/quiz7sectime3.cpp
@@ -1,12 +1,9 @@
-#include <iostream> 
-using namespace std;
-
-int findmax(int numbers[], int, int); // function declaration
-int main()
-
-{ // declare variables
-int numbers[50], numbers1[20], numbers2[20], max1, max2, n, i, a = 0;
-cout<<"Enter the the value of n: "; // input of how many numbers
-cin>>n;
-cout<<"Enter the numbers: "<<endl;
-for(i = 0; i < n ; i++) // enter all the numbers
+#include <stdio.h>
+//function starts here
+int findMax(int number[],int from,int to)
+{
+int i,largest=number[from];
+for(i=from;i<=to;i++)
+{
+ if(largest<number[i])
+largest=number[i];
