3735bc3b115075873a3f73b02f6f0b9fb23df685
Jrod595 <jsza595@gmail.com>
2022-05-13 11:19:10 -0700

    elaboration

--- quiz7/quiz7sectime2.cpp
+++ quiz7/quiz7sectime2.cpp
@@ -5,6 +5,7 @@ void printtriangle(int numbers[][3], int);
 
 int main()
 {
+    //starting array size and numbers being utilized also makeing triangle base size
     const int size=3;
     int numbers[size][size]={{0,1,2},{3,4,5},{6,7,8}};
     printtriangle(numbers,size);
@@ -16,8 +17,8 @@ void printtriangle(int numbers[][3], int size)
         for (int j=0;j<size;j++){
             if (j<=i){
                 std::cout << numbers[i][j] << ' ';
-            }
-        }
+            }//for
+        }//printtriangle
         std::cout << "\n" ;
     }
 }
