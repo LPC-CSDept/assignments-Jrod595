b3121d7aa7b16d3f40b5e876675d5e63f28f1e64
Jrod595 <jsza595@gmail.com>
2022-05-13 12:11:26 -0700

    push for q3

--- quiz7/quiz7sectime3.cpp
+++ quiz7/quiz7sectime3.cpp
@@ -7,3 +7,14 @@ for(i=from;i<=to;i++)
 {
  if(largest<number[i])
 largest=number[i];
+    }
+    return largest;
+}
+//main driver program starts here
+int main()
+{
+    const int N=10;
+    int first_largest,second_largest;
+    int number[]={10,15,0,-25,19,20,25,30,45,20};
+    //two function calls by passing array
+    first_largest = findMax(number,0,N/2-1);
