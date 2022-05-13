df9b2233609ee50ecaf9b7588c5216c8ae3db087
Jrod595 <jsza595@gmail.com>
2022-05-13 11:02:47 -0700

    push-find the difference numbers of ten

--- quiz7sectime1.cpp
+++ quiz7sectime1.cpp
@@ -10,3 +10,13 @@ int main()
   int number [N] = {10, 15, 0,-25, 19, 20, 25, 30, 45, 20};
   //cycling through each number
   for(int i=0 ; i<N ; i++){
+          
+      if(i==0 && i+1 < N) {
+      //finding any number that is difference of ten
+      if(abs( number[i] - number[i+1] ) < 10){
+        cout << number [i] << endl;
+      }
+    }else if (i==N-1 && i>0){
+      //find the number that is last with difference of ten
+      if( abs( number[i] - number[i-1] ) < 10) {
+        cout << number[i] << endl;
