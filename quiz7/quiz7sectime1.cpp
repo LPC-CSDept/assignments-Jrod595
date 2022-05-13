d1dd4d037365a3aa7f33de20178296d7dc3331c3
Jrod595 <jsza595@gmail.com>
2022-05-13 11:00:51 -0700

    push

---
+++ quiz7/quiz7sectime1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+const int N = 10;
+
+int main()
+{
+  //starting array with the numbers
+  int number [N] = {10, 15, 0,-25, 19, 20, 25, 30, 45, 20};
+  //cycling through each number
+  for(int i=0 ; i<N ; i++){
+      
+      if(i==0 && i+1 < N) {
+      //finding any number that is difference of ten
+      if(abs( number[i] - number[i+1] ) < 10){
+        cout << number [i] << endl;
+      }
+    }else if (i==N-1 && i>0){
+      //find the number that is last with difference of ten
+      if( abs( number[i] - number[i-1] ) < 10) {
+        cout << number[i] << endl;
