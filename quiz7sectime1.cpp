a596a1bc6d2f37ead07d197211fcd954b9f68df4
Jrod595 <jsza595@gmail.com>
2022-05-13 10:55:52 -0700

    uploading q1 -starting array-cyulicng through num

---
+++ quiz7sectime1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+const int N = 10;
+
+int main()
+{
+  //starting array with the numbers
+  int number [N] = {10, 15, 0,-25, 19, 20, 25, 30, 45, 20};
+  //cycling through each number
+  for(int i=0 ; i<N ; i++){
