da525c0a217655340d1b14325f581646849ab722
Jrod595 <jsza595@gmail.com>
2022-05-19 19:12:29 -0700

    push

--- Quiz9/quiz9.cpp
+++ Quiz9/quiz9.cpp
@@ -15,5 +15,32 @@ struct Employee{
    int day;
    int year;
 };
- 
-int main ()  
+ //reads employees information from the file
+
+void readFromFile(Employee employees[], int size, int& count){
+   count = 0;
+   char file[] = "employee.txt";
+   ifstream infile(file);
+   if(!infile){
+       cout<<"Error: could not find the employee.txt file.";
+       return ;
+   }
+  
+   while(count<size &&
+      
+       infile >> employees[count].ID
+       >> employees[count].fname
+       >> employees[count].lname
+       >> employees[count].sal
+       >> employees[count].department
+       >> employees[count].month
+       >> employees[count].day
+       >> employees[count].year
+       )
+       count++;
+  
+   infile.close();
+
+   
+  
+}
