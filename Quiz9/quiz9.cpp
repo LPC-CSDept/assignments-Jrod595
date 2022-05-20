66ba0621eef6c302360812c1dd5f5fe1f5459942
Jrod595 <jsza595@gmail.com>
2022-05-19 18:46:46 -0700

    push update

--- Quiz9/quiz9.cpp
+++ Quiz9/quiz9.cpp
@@ -1,18 +1,15 @@
-#include	<iostream>
-using namespace	std;
-
-struct student
-{
-	int	id;
-	string	name;
-	int	salary;
-  time_t d;
-  string department;
+#include <fstream>
+#include <string>
+ 
+using namespace std;
+ 
+struct employees {
+    string name;
+    string dept;
+    int id;
+    string startDate;
+    int salary;
 };
-
-
-int	main()
+ 
+int main ()
 {
-
-  
-}
