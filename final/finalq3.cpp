b42c98788130b3ee3fc7f96676f6679e06881da7
Jrod595 <jsza595@gmail.com>
2022-05-26 16:53:39 -0700

    push for question 3 again

--- final/finalq3.cpp
+++ final/finalq3.cpp
@@ -1,13 +1,20 @@
 #include <bits/stdc++.h> //implementation file for precomopiled header
 
-
-bool isSubset(int numbers1[], int numbers2[],
-                        int m, int n)
+//sets up method for retuns true if the elements in a pair are presented as a argument otherwise it returns false
+bool isSubset(int numbers1[], int numbers2[],  int m, int n)
 {
-        int i = 0;
+        int i = 0; // set variables
         int j = 0;
         for (i = 0; i < n; i++) {
                 for (j = 0; j < m; j++) {
                         if (numbers2[i] == numbers1[j])
                                 break;
                 }
+                 if (j == m)
+                        return 0;
+        }
+
+        
+        return 1;
+}
+
