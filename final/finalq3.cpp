0440f0f174c95a4e2fc02dc1b500e2300650646f
Jrod595 <jsza595@gmail.com>
2022-05-26 17:06:37 -0700

    push q3 added int and inputed subset

--- final/finalq3.cpp
+++ final/finalq3.cpp
@@ -17,4 +17,18 @@ bool isSubset(int numbers1[], int numbers2[],  int m, int n)
         
         return 1;
 }
+int main() //function to get returns at the end of executing 
+{
+        int numbers1[] = { 2,3,5,9,10 };
+        int numbers2[] = { 1,2,3,5,9,10,11,13,15,17 };
 
+        int m = sizeof(numbers1) / sizeof(numbers1[0]);
+        int n = sizeof(numbers2) / sizeof(numbers2[0]);
+if (isSubset(numbers1, numbers2, m, n))//checking for subsets between numbers1 and numbers2
+                printf("numbers1[] is subset of numbers1[] ");
+        else
+                printf("numbers2[] is not a subset of numbers1[]");
+
+        getchar();
+        return 0;
+}
